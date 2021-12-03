639df7b8ed2d47c9e2a91c2d64169eaa960be3b9
neslihankara <neslihankara40@gmail.com>
2021-12-03 21:13:31 +0300

    Add Button to Topic Card and update style

--- components/topic-card.tsx
+++ components/topic-card.tsx
@@ -8,6 +8,10 @@ type IProps = {
   bannerPath: string
 };
 
+const StyledDiv = styled.div({
+
+})
+
 const StyledTopicCard = styled.div({
   boxShadow: "0px 3px 4px grey",
   maxWidth: "450px"
@@ -17,11 +21,24 @@ const StyledBanner = styled.img({
   width: "100%",
 })
 
+const StyledButton = styled.button({
+  backgroundColor: "white",
+  border: "0px",
+  cursor: "pointer",
+})
+
 const Topic: React.FC<IProps> = ({ topic, resources, bannerPath }) => (
   <StyledTopicCard>
-    <StyledBanner src={bannerPath} alt=""/>
-    <div className="summary"> <h1>{topic}</h1>
-    <p>Total Resources: {resources.length}</p></div>
+    <StyledBanner src={bannerPath} alt="" />
+    <div className="content">
+      <div className="summary">
+        <div className="course-name" id="course">
+          <h2>{topic}</h2>
+        </div>
+        <StyledButton>+</StyledButton>
+      </div>
+      <p>Total Resources: {resources.length}</p>
+    </div>
   </StyledTopicCard>
 )
 
