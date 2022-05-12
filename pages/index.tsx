fb67a11d4350eabbe8ee5dd691f88741171e0c29
Neslihan Kara <neslihankara40@gmail.com>
2022-05-12 15:40:41 +0300

    add id to script

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,19 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <div className="container">
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `(function(h,o,t,j,a,r){
-                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                        h._hjSettings={hjid:2966870,hjsv:6};
-                        a=o.getElementsByTagName('head')[0];
-                        r=o.createElement('script');r.async=1;
-                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                        a.appendChild(r);
-                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-          }}
-        />
+      <Script
+        id="hotjar-script"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+                      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                      h._hjSettings={hjid:2966870,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
       <div className="header">
         <h1>Resources for Programming with Nes Community</h1>
         <p>We've been building this site in the Live Room of Vibely. You can access the Live Room from&nbsp;
